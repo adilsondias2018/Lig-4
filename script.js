b093409f37f1e3ff9ff41a2e0c43b707662d975f
Adilson Dias <adilsondias@outlook.com>
2020-09-21 21:50:33 -0300

    step1 finalizado para para Desktop

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+const playerUm = document.createElement('img')
+const playDois =document.createElement('img')
 
 
+const linha1 = document.getElementById('celula1')
+playerUm.id = 'yellowCircle'
+playerUm.src = './img/first.png'
+
+const linha2 = document.getElementById('celula2')
+// playerDois.id = 'blackCircle'
+// playerDois.src = './img/second.png'
+
+linha1.appendChild(playerUm)
 
 
