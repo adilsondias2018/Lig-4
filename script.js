619af8b9eb980c347972d052df3abaa517061aa7
Adilson Dias <adilsondias@outlook.com>
2020-09-22 11:51:02 -0300

    Versao atualizada para Desktop

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const playerUm = document.createElement('img')
-const playDois =document.createElement('img')
+// Images
+const playerOne = document.createElement('img')
+const playerTwo = document.createElement('img')
 
 
-const linha1 = document.getElementById('celula1')
-playerUm.id = 'yellowCircle'
-playerUm.src = './img/first.png'
+playerOne.id  = 'yellowCircle'
+playerOne.src = './img/first.png'
 
-const linha2 = document.getElementById('celula2')
-// playerDois.id = 'blackCircle'
-// playerDois.src = './img/second.png'
-
-linha1.appendChild(playerUm)
+playerTwo.id  = 'blackCircle'
+playerTwo.src = './img/second.png'
 
 
